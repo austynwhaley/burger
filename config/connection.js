fd05e93493bc3d07aa9e3d570a9d37917fa4513a
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-01-26 22:18:31 -0500

    JawsDB connection added

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "12Aw3309!",
     database: "burgers_db"
   });
+}
   
 
   connection.connect(function(err) {
