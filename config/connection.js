97b5fcbfadeb50cc7b5c61d9f4996b96e5ad38b8
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2021-01-26 22:34:47 -0500

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -15,12 +15,6 @@ if (process.env.JAWSDB_URL) {
 }
   
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+connection.connect();
   
-  module.exports = connection;
+module.exports = connection;
